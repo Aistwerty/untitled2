fba8a209de33544bf60b0a08380a06b245daab1e
Aleksander <a.stupenkov@mail.ru>
2021-03-30 12:04:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Route, BrowserRouter, NavLink} from "react-router-dom"
+function Menu(){
+    return(
+
+            <nav className="nav">
+                <NavLink className="nav-link active" aria-current="page" to="/">Главная</NavLink>
+                <NavLink className="nav-link" to="/about">О нас</NavLink>
+                <NavLink className="nav-link" to="/contact-us">Контакты</NavLink>
+            </nav>
+    )
+}
+function ContactUs(){
+    return (
+        <div className='container-sm my-1'>
+            <div className="mb-3"><input type="text" className="form-control"/></div>
+            <div className="mb-3"><input type="text" className='form-control'/></div>
+            <div className="mb-3"><textarea type="text" className='form-control'/></div>
+            <div className="mb-3"><input type="submit" className='form-control btn btn-light'/></div>
+        </div>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Menu/>
+            <Route exact path='/' render={()=><h3>Главная страница</h3>} />
+            <Route path='/about' render={()=><h3>О нас</h3>} />
+            <Route path='/contact-us' render={()=><ContactUs/>} />
+        </BrowserRouter>
     </div>
   );
 }
